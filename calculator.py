6b2f6f4baaf446543599f3cc1a7f137680c402b0
KellieGrieneisen <kellie_grieneisen@aol.com>
2021-04-28 21:05:34 +0000

    Create infinite while loop with user input, tokenize input, add breaka
    Co-authored by: Maura Thompson <thomaura@gmail.com>

--- calculator.py
+++ calculator.py
@@ -4,4 +4,26 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
                         power, mod, )
 
 
-# Replace this with your code
+# if the token is "add" 
+#   then call the add function with the remaining two tokens
+
+# repeat forever:
+
+while True:
+#     read input
+    equation = input("Enter your equation here: ")  
+#     tokenize input
+    tokens = equation.split(' ')
+    
+#         if the first token is "q":
+    first = tokens[0]
+    print(first)
+    if first =='q':
+        break
+    print(tokens)
+
+#             quit
+#         else:
+#             (decide which math function to call based on first token)
+#             if the first token is 'pow':
+#                   call the power function with the other two tokens
