e569390fed32edb2cdb79b0333bad4313d686406
KellieGrieneisen <kellie_grieneisen@aol.com>
2021-04-28 21:57:05 +0000

    Created call for multiplication, division, square and cube
    Co-authored by: Maura Thompson ,thomaura@gmail.com>

--- calculator.py
+++ calculator.py
@@ -18,7 +18,12 @@ while True:
 #         if the first token is "q":
     calc = tokens[0]
     num1 = int(tokens[1])
-    num2 = int(tokens[2])
+    
+    if len(tokens) < 3:
+        num2 ='0'
+    else:
+        num2 = int(tokens[2]) 
+
     if calc =='q':
         break
     #print(tokens)
@@ -27,7 +32,26 @@ while True:
         addition = add(float(num1), float(num2))
         print(addition)
         #print output of add function
-
+    elif calc == '-' or calc == 'subtract':
+        #call subtraction function on token[1] and token[2]
+        subtraction = subtract(float(num1), float(num2))
+        print(subtraction)
+    elif calc == '*' or calc == 'multiply':
+        #call multiplication function on token[1] and token[2]
+        multiplication = multiply(float(num1), float(num2))
+        print(multiplication)
+    elif calc == '/' or calc == 'divide':
+        #call division function on token[1] and token[2]
+        division = divide(float(num1), float(num2))
+        print(division)
+    elif calc == 'square':
+        #call square function on token[1] and token[2]
+        squared = square(float(num1))
+        print(squared)
+    elif calc == 'cube':
+        #call square function on token[1] and token[2]
+        cubed = cube(float(num1))
+        print(cubed)
 
 
     
